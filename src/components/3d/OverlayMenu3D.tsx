cf61290ee7cc409c3fca4592b4c002fdfc7ab713
okorion <ok.or.orion@gmail.com>
2025-04-14 21:43:41 +0900

    feat: OverlayMenu3D icon 추가 기능

--- src/components/3d/OverlayMenu3D.tsx
+++ src/components/3d/OverlayMenu3D.tsx
@@ -1,13 +1,22 @@
 import { Html } from "@react-three/drei";
+import { ReactNode } from "react";
 
 interface Props {
   position?: [number, number, number];
   label: string;
   url?: string;
+  icon?: ReactNode;
   onHoverChange: (isHovered: boolean) => void;
 }
 
-const InteractiveLabel = ({ position, label, url, onHoverChange }: Props) => {
+const InteractiveLabel = ({
+  position,
+  label,
+  url,
+  icon,
+  onHoverChange,
+}: Props) => {
+  const iconSize = 40;
   const handleClick = () => {
     if (url) window.open(url, "_blank");
   };
@@ -15,11 +24,16 @@ const InteractiveLabel = ({ position, label, url, onHoverChange }: Props) => {
   return (
     <Html position={position} scale={0.05} transform>
       <div
-        className="opacity-70 hover:opacity-100 transition-opacity duration-200 text-white text-3xl font-bold select-none pointer-events-auto cursor-pointer"
+        className="flex items-center gap-2 opacity-70 hover:opacity-100 transition-opacity duration-200 text-white text-3xl font-bold select-none pointer-events-auto cursor-pointer"
         onMouseEnter={() => onHoverChange(true)}
         onMouseLeave={() => onHoverChange(false)}
         onClick={handleClick}
       >
+        {icon && typeof icon === "string" ? (
+          <img src={icon} alt={label} width={iconSize} height={iconSize} />
+        ) : (
+          icon
+        )}
         {label}
       </div>
     </Html>
@@ -41,24 +55,28 @@ export const OverlayMenu3D = ({ setHovered }: OverlayMenu3DProps) => {
         position={[OverlayMenuX, OverlayMenuY, OverlayMenuZ]}
         label="okorion"
         url="https://github.com/okorion"
+        icon="/icons/home.svg"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX - 0.5, OverlayMenuY - 0.2, OverlayMenuZ]}
         label="GitHub"
         url="https://github.com/okorion"
+        icon="/icons/github.svg"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX, OverlayMenuY - 0.2, OverlayMenuZ]}
         label="Velog"
         url="https://velog.io/@okorion"
+        icon="/icons/velog.svg"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX + 0.5, OverlayMenuY - 0.2, OverlayMenuZ]}
         label="Portfolio(Notion)"
         url="https://okorion.notion.site/Portfolio-1d50242aaedf80988f93f5af21fe0304"
+        icon="/icons/notion.svg"
         onHoverChange={setHovered}
       />
     </>
