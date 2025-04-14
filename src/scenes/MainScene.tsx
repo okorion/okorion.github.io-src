ee921923ccc2def65464d6dd6b22029b76d65139
okorion <ok.or.orion@gmail.com>
2025-04-14 22:07:28 +0900

    feat: 마우스 휠 이벤트 Scene 회전 추가

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -2,21 +2,28 @@ import { Canvas } from "@react-three/fiber";
 import { CameraController } from "../components/3d/Camera/CameraController";
 import { Floor } from "../components/3d/Floor";
 import { LightController } from "../components/3d/LightController";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ArcaneWillow } from "../components/3d/ArcaneWillow";
 import { ChromeFigure } from "../components/3d/ChromeFigure";
 import { OverlayMenu3D } from "../components/3d/OverlayMenu3D";
+import { useScrollRotateScene } from "./useScrollRotateScene";
+import { Group, Object3DEventMap } from "three";
 
 const MainScene = () => {
   const [isHovered, setHovered] = useState(false);
+  const sceneRef = useRef<Group<Object3DEventMap> | null>(null);
+
+  useScrollRotateScene(sceneRef, 0.05);
 
   return (
     <Canvas style={{ background: "#949494" }} shadows>
-      <OverlayMenu3D setHovered={setHovered} />
+      <group ref={sceneRef}>
+        <OverlayMenu3D setHovered={setHovered} />
 
-      <ChromeFigure />
-      <ArcaneWillow />
-      <Floor />
+        <ChromeFigure />
+        <ArcaneWillow />
+        <Floor />
+      </group>
 
       <CameraController />
       <LightController isHovered={isHovered} />
---
+++ src/scenes/useScrollRotateScene.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { Group, Object3DEventMap } from "three";
+
+export const useScrollRotateScene = (
+  sceneRef: React.RefObject<Group<Object3DEventMap> | null>,
+  step: number = 0.05
+) => {
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      const delta = e.deltaY > 0 ? -step : step;
+
+      if (sceneRef.current) {
+        sceneRef.current.rotation.y += delta;
+      }
+    };
+
+    window.addEventListener("wheel", handleWheel);
+    return () => window.removeEventListener("wheel", handleWheel);
+  }, [sceneRef, step]);
+};
