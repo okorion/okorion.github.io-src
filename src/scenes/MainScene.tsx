3f535fcf1eb3920d6aa4dfae4eccf666f4c5ed4c
okorion <ok.or.orion@gmail.com>
2025-04-14 20:01:22 +0900

    feat: MainScene 3D 에셋 추가(나무, 모형, 바닥)

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,14 +1,36 @@
 import { Canvas } from "@react-three/fiber";
 import CameraController from "../components/3d/CameraController";
-import Box from "../components/3d/Box";
+import ModelLoader from "../loader/ModelLoader";
+import { FilledCirclePoints } from "./FilledCirclePoints";
 
 const MainScene = () => {
   return (
-    <Canvas fallback={<div>Sorry no WebGL supported!</div>}>
-      <Box position={[-1, 0, 0]} />
-      <Box position={[1, 0, 0]} />
-      <ambientLight intensity={0.5} />
+    <Canvas style={{ background: "#949494" }} shadows>
+      <ambientLight intensity={4} />
       <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+
+      <ModelLoader
+        path="/src/models/ChromeFigure.glb"
+        position={[0, 0, 0]}
+        scale={3}
+      />
+      <ModelLoader
+        path="/src/models/ArcaneWillow.glb"
+        scale={1}
+        interpolate
+        usePoints
+      />
+      <ModelLoader
+        path="/src/models/ArcaneWillow.glb"
+        scale={0.99}
+        opacity={0.12}
+      />
+
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
+        <circleGeometry args={[10, 64]} />
+        <meshStandardMaterial color="#bbbbbb" />
+      </mesh>
+      <FilledCirclePoints radius={10} rings={100} segmentsPerRing={100} />
 
       <CameraController />
     </Canvas>
