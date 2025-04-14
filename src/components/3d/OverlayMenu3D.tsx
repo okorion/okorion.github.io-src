98d55fa302ce9c7966492b65ac2747356ee92f0f
okorion <ok.or.orion@gmail.com>
2025-04-14 21:21:39 +0900

    feat: overlayMenu3D url 기능 추가 및 적용

--- src/components/3d/OverlayMenu3D.tsx
+++ src/components/3d/OverlayMenu3D.tsx
@@ -3,16 +3,22 @@ import { Html } from "@react-three/drei";
 interface Props {
   position?: [number, number, number];
   label: string;
+  url?: string;
   onHoverChange: (isHovered: boolean) => void;
 }
 
-const InteractiveLabel = ({ position, label, onHoverChange }: Props) => {
+const InteractiveLabel = ({ position, label, url, onHoverChange }: Props) => {
+  const handleClick = () => {
+    if (url) window.open(url, "_blank");
+  };
+
   return (
     <Html position={position} scale={0.05} transform>
       <div
         className="opacity-70 hover:opacity-100 transition-opacity duration-200 text-white text-3xl font-bold select-none pointer-events-auto cursor-pointer"
         onMouseEnter={() => onHoverChange(true)}
         onMouseLeave={() => onHoverChange(false)}
+        onClick={handleClick}
       >
         {label}
       </div>
@@ -34,21 +40,25 @@ export const OverlayMenu3D = ({ setHovered }: OverlayMenu3DProps) => {
       <InteractiveLabel
         position={[OverlayMenuX, OverlayMenuY, OverlayMenuZ]}
         label="okorion"
+        url="https://github.com/okorion"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX - 0.5, OverlayMenuY - 0.2, OverlayMenuZ]}
         label="GitHub"
+        url="https://github.com/okorion"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX, OverlayMenuY - 0.2, OverlayMenuZ]}
         label="Velog"
+        url="https://velog.io/@okorion"
         onHoverChange={setHovered}
       />
       <InteractiveLabel
         position={[OverlayMenuX + 0.5, OverlayMenuY - 0.2, OverlayMenuZ]}
-        label="Portfolio"
+        label="Portfolio(Notion)"
+        url="https://okorion.notion.site/Portfolio-1d50242aaedf80988f93f5af21fe0304"
         onHoverChange={setHovered}
       />
     </>
